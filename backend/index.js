b67202273bffca0caa15d8ae1b5434131e788f5b
ndhnws14 <donghonew17@gmail.com>
2026-03-02 14:01:34 +0700

    "deploy fe"

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://your-frontend.vercel.app', 
     credentials: true
 }));
 
