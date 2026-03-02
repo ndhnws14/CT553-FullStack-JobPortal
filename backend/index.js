198aa31c6a1e608b95f41eb36e4345a27271fd29
ndhnws14 <donghonew17@gmail.com>
2026-03-02 14:03:07 +0700

    "fix"

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'https://your-frontend.vercel.app', 
+    origin: 'https://ct-553-full-stack-job-portal.vercel.app', 
     credentials: true
 }));
 
