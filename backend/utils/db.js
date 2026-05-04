1c6294e23f6d6bf72ae1e4b6c848d9cadc9cc022
ndhnws14 <donghonew17@gmail.com>
2026-05-04 20:12:12 +0700

    update cuối

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(process.env.MONGO_COM);
+        await mongoose.connect(process.env.MONGO_COM); // xem lai
         console.log("mongodb connected successfully");
     } catch (error) {
         console.log(error);
