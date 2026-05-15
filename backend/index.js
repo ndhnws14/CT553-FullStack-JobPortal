6eedfc4a358c4e1b8976652e848724077453359d
ndhnws14 <donghonew17@gmail.com>
2026-05-15 23:38:48 +0700

    fix

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://your-vercel-app.vercel.app"
+        "https://geekjobs-lilac.vercel.app"
     ],
     credentials: true
 }));
@@ -57,7 +57,7 @@ export const io = new Server(server, {
   cors: {
     origin: [
         "http://localhost:5173",
-        "https://your-vercel-app.vercel.app"
+        "https://geekjobs-lilac.vercel.app"
     ],
     credentials: true
   }
